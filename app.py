0c4ccede03196c991a5aa8a217b7c6c1b246d35a
RoiFolstag <36136669+RoiFolstag@users.noreply.github.com>
2022-12-01 18:42:22 +0100

    Adding audio file and translating working

--- app.py
+++ app.py
@@ -5,25 +5,39 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    transcript = ""
+    transcript = "*waiting for audio*"
     if request.method == "POST":
+        #has_changed = 0
+        transcript = "*waiting for audio*"
+        print("Variable transcript set to: (BEGIN):", transcript)
         print("FORM DATA RECEIVED")
 
         if "file" not in request.files:
+            print("No file?")
             return redirect(request.url)
 
         file = request.files["file"]
         if file.filename == "":
+            print("No filename?")
             return redirect(request.url)
 
         if file:
+            print("File", file.filename, "has been loaded")
             recognizer = sr.Recognizer()
             audioFile = sr.AudioFile(file)
             with audioFile as source:
                 data = recognizer.record(source)
             transcript = recognizer.recognize_google(data, key=None)
-
+            has_changed = 1
+        print("Variable transcript set to: (END):", transcript)
+            
+    #if has_changed == 0:
+        #transcript="*waiting for audio*"
+    print("Variable transcript set to: (EOF):", transcript)
     return render_template('index.html', transcript=transcript)
 
+    
+    
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
+    
