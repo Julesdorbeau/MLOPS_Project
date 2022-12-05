a5555103fee00d4ddd5799826dcdf9a443057f83
noe.jenn-treyer <noe.jenn-treyer@epita.com>
2022-12-06 00:12:31 +0100

    Working buttons and file system

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, request, redirect
 import speech_recognition as sr
+import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     transcript = "*waiting for audio*"
+    print("Variable transcript set to: (BEGIN):", transcript)
+    
     if request.method == "POST":
-        #has_changed = 0
-        transcript = "*waiting for audio*"
-        print("Variable transcript set to: (BEGIN):", transcript)
-        print("FORM DATA RECEIVED")
-
         if "file" not in request.files:
             print("No file?")
             return redirect(request.url)
@@ -23,21 +21,38 @@ def index():
 
         if file:
             print("File", file.filename, "has been loaded")
-            recognizer = sr.Recognizer()
-            audioFile = sr.AudioFile(file)
-            with audioFile as source:
-                data = recognizer.record(source)
-            transcript = recognizer.recognize_google(data, key=None)
-            has_changed = 1
-        print("Variable transcript set to: (END):", transcript)
+            data = request.files['file'].read()
+            try:
+                os.remove('input/myfile.wav')
+            except OSError:
+                pass
+            with open('input/myfile.wav', mode='bx') as f:
+                f.write(data)
+                print("Audio file created in input/ file")
+            r_form = request.form['submit_button']
+            if r_form == 'Default (TFlite)':
+                print(r_form)
+                return render_template('index.html', transcript=r_form)
+
+            elif r_form == 'Default (h5)':
+                print(r_form)
+                return render_template('index.html', transcript=r_form)
             
-    #if has_changed == 0:
-        #transcript="*waiting for audio*"
-    print("Variable transcript set to: (EOF):", transcript)
+            elif r_form == 'Experimental (TFlite)':
+                print(r_form)
+                return render_template('index.html', transcript=r_form)
+            
+            elif r_form == 'Float 16 (TFlite)':
+                print(r_form)
+                return render_template('index.html', transcript=r_form)
+            
+            elif r_form == 'No optimization (TFlite)':
+                print(r_form)
+                return render_template('index.html', transcript=r_form)
+
     return render_template('index.html', transcript=transcript)
 
-    
-    
+
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
     
